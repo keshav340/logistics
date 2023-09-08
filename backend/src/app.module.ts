90eb630fdc7615ad5ffb932ac51cffc2be2c6430
Keshav Sharma <61562452+keshav340@users.noreply.github.com>
2023-09-08 13:16:20 +0530

    started project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { MyResolver } from './app.resolver';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: 'src/schema.gql', // Set the file name here
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin',
+      database: 'logistic',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  providers: [MyResolver],
 })
 export class AppModule {}
